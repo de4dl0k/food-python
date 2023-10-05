a7f6e686d5a52a48db5d01863e72455fddecbe52
de4dl0ck <sarangkodiyeri@gmail.com>
2023-10-05 12:12:01 +0530

    saved

---
+++ foodproject/foodapp/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . models import Food
+from . forms import FoodForm
+
+
+# Create your views here.
+
+
+def index(request):
+    food = Food.objects.all()
+    context = {
+        'food_list': food
+    }
+    return render(request, 'index.html', context)
+
+
+def detail(request, food_id):
+    food = Food.objects.get(id=food_id)
+    return render(request, "detail.html", {'food': food})
+
+
+def add_food(request):
+    if request.method == 'POST':
+        name = request.POST.get('name',)
+        desc = request.POST.get('desc',)
+        price = request.POST.get('price',)
+        img = request.FILES['img']
+        food = Food(name=name, desc=desc, price=price, img=img)
+        food.save()
+    return render(request, 'add.html')
+
+
+def update(request, id):
+    food = Food.objects.get(id=id)
+    form = FoodForm(request.POST or None, request.FILES, instance=food)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'edit.html', {'form': form, 'food': food})
+
+
+def delete(request, id):
+    if request.method == 'POST':
+        food = Food.objects.get(id=id)
+        food.delete()
+        return redirect('/')
+    return render(request, 'delete.html')
+
+
